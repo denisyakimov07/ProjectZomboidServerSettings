012901a396767cb5493d70d881d359397b39a951
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-02-13 11:09:31 -0500

    Add files via upload

---
+++ Yanis07_spawnregions.lua
@@ -0,0 +1,17 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Raven Creek, KY", file = "steamapps/workshop/content/108600/2196102849/mods/RavenCreek/media/maps/RavenCreek/spawnpoints.lua" },
+		{ name = "Blackwood, KY", file = "steamapps/workshop/content/108600/2536865912/mods/Blackwood/media/maps/Blackwood/spawnpoints.lua" },
+		{ name = "Chernaville, KY", file = "steamapps/workshop/content/108600/2734683361/mods/Chernaville/media/maps/chernaville/spawnpoints.lua" },
+		{ name = "Fort Rock Ridge", file = "steamapps/workshop/content/108600/2806456685/mods/Fort Rock Ridge/media/maps/Fort Rock Ridge/spawnpoints.lua" },
+		{ name = "New Ekron", file = "steamapps/workshop/content/108600/2712480036/mods/Fort Rock Ridge/media/maps/NewEkron/spawnpoints.lua" },
+   
+		-- Uncomment the line below to add a custom spawnpoint for this server.
+--		--{ name = "Twiggy's Bar", serverfile = "Yanis07_spawnpoints.lua" },
+	}
+end
+
