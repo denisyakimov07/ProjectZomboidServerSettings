e4b64b41c16d63b630816625748b298d735933ec
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-02-15 21:07:00 -0500

    Update Yanis07_spawnregions.lua

--- Yanis07_spawnregions.lua
+++ Yanis07_spawnregions.lua
@@ -9,6 +9,7 @@ function SpawnRegions()
 		{ name = "Chernaville, KY", file = "steamapps/workshop/content/108600/2734683361/mods/Chernaville/media/maps/chernaville/spawnpoints.lua" },
 		{ name = "Fort Rock Ridge", file = "steamapps/workshop/content/108600/2806456685/mods/Fort Rock Ridge/media/maps/Fort Rock Ridge/spawnpoints.lua" },
 		{ name = "New Ekron", file = "steamapps/workshop/content/108600/2712480036/mods/Fort Rock Ridge/media/maps/NewEkron/spawnpoints.lua" },
+		{ name = "New Ekron", file = "steamapps/workshop/content/108600/2782415851/mods/coryerdon/media/maps/coryerdon/spawnpoints.lua" },
    
 		-- Uncomment the line below to add a custom spawnpoint for this server.
 --		--{ name = "Twiggy's Bar", serverfile = "Yanis07_spawnpoints.lua" },
